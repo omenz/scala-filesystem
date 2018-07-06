4486652de4f2f6a354585ea8f7b5305d98d1cbb9
Alexander Krasovsky <ak@weissenrieder.com>
2018-07-06 17:58:20 +0300

    echo command

--- src/com/omenz/commands/Echo.scala
+++ src/com/omenz/commands/Echo.scala
@@ -1,4 +1,5 @@
 package com.omenz.commands
+import com.omenz.files.{Directory, File}
 import com.omenz.filesystem.State
 
 import scala.annotation.tailrec
@@ -24,8 +25,37 @@ class Echo(args: List[String]) extends Command {
     }
   }
 
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String],
+                       contents: String, append: Boolean): Directory = {
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+
+      if (dirEntry == null)
+        currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+      else if (dirEntry.isDirectory) currentDirectory
+      else
+        if (append) currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+        else currentDirectory.replaceEntry(path.head, dirEntry.asFile.setContents(contents))
+    } else {
+      val nextDirectory = currentDirectory.findEntry(path.head).asDirectory
+      val newNextDirectory = getRootAfterEcho(nextDirectory, path.tail, contents, append)
+
+      if (newNextDirectory == nextDirectory) currentDirectory
+      else currentDirectory.replaceEntry(path.head, newNextDirectory)
+    }
+  }
+
   def doEcho(state: State, content: String, filename: String, append: Boolean): State = {
-    ???
+    if (filename.contains(Directory.SEPARATOR))
+      state.setMessage("Filename must not contain separator")
+    else {
+      val newRoot: Directory = ???
+      if (newRoot == state.root)
+        state.setMessage(s"$filename: no such file")
+      else
+        State(newRoot, newRoot.findDescendant(state.wd.getAllFoldersInPath))
+    }
   }
 
   def createContent(args: List[String], topIndex: Int): String = {
